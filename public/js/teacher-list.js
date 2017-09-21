edb02e1fd5768822502af680514b87497edcfbc8
fengpanlin <15001047614@163.com>
2017-09-21 23:57:57 +0800

    完成启用和注销功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -8,6 +8,33 @@ define(['jquery','template'],function($,template){
       console.log(data.result);
       var html = template('teacherTpl',{list:data.result});
       $('#teacherInfo').html(html);
+
+
+      // 注销启用讲师功能
+      $('.enabledOrDisabled').on('click',function(){
+        // var td = $(this).parent('td');
+        var that = this;
+        var td = $(this).closest('td');// closest() 某个元素最近的父元素 
+        var tcId = td.attr('data-tcId');
+        var tcStaus = td.attr('data-status');
+        $.ajax({
+          type:'post',
+          url:'/api/teacher/handle',
+          data:{tc_id:tcId,tc_status:tcStaus},
+          dataType:'json',
+          success:function(data){
+            if (data.code == 200) {
+              td.attr('data-status',data.result.tc_status);
+              if (data.result.tc_status == 1) {
+                $(that).text('启用');
+              } else {
+                $(that).text('注销');
+              }
+            }
+          }
+        });
+      });
+      
     }
   });
 });
