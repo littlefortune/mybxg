92d076eeb943280904087120276bb037e10c33f6
fengpanlin <15001047614@163.com>
2017-09-24 13:11:15 +0800

    完成讲师添加和编辑功能

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -12,12 +12,32 @@ define(['jquery','template','util'],function($,template,util){
         // 解析数据，渲染页面
         var html = template('teacherTpl',data.result);
         $('#teacherInfo').html(html);
-      }
+        // 处理表单提交功能
+        submitForm('/api/teacher/update');
+      } 
     });
   } else {
     // 添加功能
-     var html = template('teacherTpl',{operate:'添加讲师'});
-     $('#teacherInfo').html(html);
+    var html = template('teacherTpl',{operate:'添加讲师'});
+    $('#teacherInfo').html(html);
+    submitForm('/api/teacher/add');
+  }
+
+  // 实现表单提交
+  function submitForm(url){
+    $('#teacherBtn').on('click',function(){
+      $.ajax({
+        type:'post',
+        url:url,
+        data:$('#teacherForm').serialize(),
+        dataType:'json',
+        success:function(data){
+          if (data.code == 200) {
+            location.href = '/teacher/list';
+          }
+        }
+      });
+    });
   }
 
 
