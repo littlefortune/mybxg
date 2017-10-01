15a5e384cb1466f6efa22a470d09d2fa0ef6b7f6
fengpanlin <15001047614@163.com>
2017-10-01 16:28:55 +0800

    完成左侧导航菜单选中

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,4 +1,6 @@
-define(['jquery','template','bootstrap'],function($,template){
+define(['jquery','template','util','bootstrap'],function($,template,util){
+  // 设置导航栏选中
+  util.setMenu(location.pathname);
   // 调用接口获取所有的讲师数据
   $.ajax({
     url:'/api/teacher',
